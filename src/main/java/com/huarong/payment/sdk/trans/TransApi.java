046341e688abbf00cd8839374ebaa91ac20cb135
walden1993 <940417907@qq.com>
2017-07-30 09:52:04 +0800

    增加注释

--- src/main/java/com/huarong/payment/sdk/trans/TransApi.java
+++ src/main/java/com/huarong/payment/sdk/trans/TransApi.java
@@ -2,5 +2,11 @@ package com.huarong.payment.sdk.trans;
 
 
 public interface TransApi<T> {
+	/**
+	 * 通用的交易方法
+	 * @param t
+	 * @return
+	 * @throws Exception
+	 */
 	public T trans(T t) throws Exception;
 }
